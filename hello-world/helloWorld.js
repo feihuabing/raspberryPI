ae68bdc961bc60bc16f54dc8d17851b1e714989e
feihuabing <1109475083@qq.com>
2019-03-04 16:23:25 +0800

    ·
    1

--- hello-world/helloWorld.js
+++ hello-world/helloWorld.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const led = require('./led.js')
 const app = express();
 app.get('/temp',(req,res)=>{
     res.send('10')
 })
 app.get('/hum',(req,res)=>{
     res.send('48')
+})
+app.get('/on',function (req,res){
+    led.on();
+})
+app.get('/off',function (req,res){
+    led.off();
 })
 app.listen(3000,()=>{
     console.log('server listening on port 3000');
