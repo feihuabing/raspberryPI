d7e5a45d20879adc3093863ad906ed80b86b0605
feihuabing <1109475083@qq.com>
2019-03-04 11:51:47 +0800

    1
    1

--- hello-world/helloWorld.js
+++ hello-world/helloWorld.js
@@ -4,7 +4,7 @@ app.get('/temp',(req,res)=>{
     res.send('10')
 })
 app.get('/hum',(req,res)=>{
-    res.send(48)
+    res.send('48')
 })
 app.listen(3000,()=>{
     console.log('server listening on port 3000');
