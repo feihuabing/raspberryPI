542c95d59e774860fd16773cf541ef90306e2c87
feihuabing <1109475083@qq.com>
2019-03-06 10:54:18 +0800

    1
    1

---
+++ hello-world/rpio.js
@@ -0,0 +1,11 @@
+var Gpio = require("rpio2");
+var led = new Gpio(36);  //创建 P36 引脚
+
+led.open(Gpio.OUTPUT, Gpio.LOW); //设置为 OUTPUT、默认低电平
+
+for(var i = 0; i < 10; i++){
+    led.toggle();  //切换 led 的电平状态
+    led.sleep(500);  //等待 500ms
+}
+
+led.close();
